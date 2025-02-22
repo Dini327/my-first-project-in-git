ba3f2373ac46c1af0914f851865aafe5a58a0a79
Dini327 <dinibraverman@gmail.com>
2025-02-22 20:47:29 +0200

    Description of changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express=require('express');
 var app=express()
 
 app.listen(3020,function(){
-    console.log('Example app listening o nport 3000!');
+    console.log('Example app listening on nport 3000!');
 })
 
 app.get('/',function(req,res){
